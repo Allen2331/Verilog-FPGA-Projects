ec844b76c109ae4df8b558f00db55898cd982e2f
Allen2331 <55219200+Allen2331@users.noreply.github.com>
2025-07-22 13:56:28 -0700

    Update instruction_memory.sv

--- RISC-V RV32I Processor/Modules/instruction_memory.sv
+++ RISC-V RV32I Processor/Modules/instruction_memory.sv
@@ -25,13 +25,15 @@ module instruction_memory #(parameter mem_size = 1024, parameter mem_depth = mem
     output logic [31:0] instruction_out
     );
     
-    logic [31:0] instruction_memory [mem_depth-1:0];
-    
-    parameter instructions = ".../instructions.hex"; // Add data path to instruction hex file
-    
+    logic [31:0] memory [mem_depth-1:0];
+        
     initial begin
-        $readmemh(instructions, instruction_memory);
+        memory[0] = 32'h002081b3; // add x3, x1, x2
+        memory[1] = 32'h0020c1b3; // or  x3, x1, x2
+        memory[2]  = 32'h00500113; // addi x2, x0, 5
+        memory[3] = 32'h00012383; // lw x7, 0(x2)
+        memory[4] = 32'h00712023; // sw x7, 0(x2)
     end
     
-    assign instruction_out = instruction_memory[pc[31:2]];    
+    assign instruction_out = memory[pc[31:2]];    
 endmodule
